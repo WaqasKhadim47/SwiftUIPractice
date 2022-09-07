c19f2fc4b7c1aca0f008f2168440971cb3377449
waqas.khadim <waqas.khadim@invozone.com>
2022-09-07 15:16:57 +0500

    Initial Commit

---
+++ SwiftUiPractice/SwiftUiPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUiPracticeApp.swift
+//  SwiftUiPractice
+//
+//  Created by Waqas Khadim on 07/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUiPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
